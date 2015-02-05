64f2469400f36f035283d39a1e7b04cf5c1a8ea2
Ladar Levison <ladar@users.noreply.github.com>
2015-02-05 04:36:11 -0600

    Removed absolute paths fixed version num.
    Changed version number to match actual version... 6.0.1...

--- scripts/builders/build.prep.sh
+++ scripts/builders/build.prep.sh
@@ -9,8 +9,8 @@ MAGMA_ROOT=`pwd`
 
 rm -f $MAGMA_ROOT/engine/status/build.h
 
-COMMIT_ID=`/usr/bin/git log --format="%H" -n 1 | cut -c33-40`
-printf "#define MAGMA_BUILD \"5.2."$COMMIT_ID"\"\n" > $MAGMA_ROOT/engine/status/build.h
-/bin/date +'#define MAGMA_STAMP "%Y%m%d.%H%M"' >> $MAGMA_ROOT/engine/status/build.h
+COMMIT_ID=`git log --format="%H" -n 1 | cut -c33-40`
+printf "#define MAGMA_BUILD \"6.0.1-"$COMMIT_ID"\"\n" > $MAGMA_ROOT/engine/status/build.h
+date +'#define MAGMA_STAMP "%Y%m%d.%H%M"' >> $MAGMA_ROOT/engine/status/build.h
 
 sleep 1

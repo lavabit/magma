2d1bbeb04bb9462761429a89b77d5b28b7cd2d92
Zachary Michaels <mikezackles@gmail.com>
2015-06-23 21:21:35 -0400

    Temporary fix for hardcoded paths to corpus

--- check/magma_check.h
+++ check/magma_check.h
@@ -64,15 +64,15 @@ extern int case_timeout;
 #define TANK_CHECK_DATA_UNUM 1l
 #define TANK_CHECK_DATA_MTHREADS 2 // Disabled
 #define TANK_CHECK_DATA_CLEANUP true
-#define TANK_CHECK_DATA_PATH "res/corpus/"
+#define TANK_CHECK_DATA_PATH "../res/corpus/"
 
 #define DSPAM_CHECK_SIZE_MIN 1024
 #define DSPAM_CHECK_SIZE_MAX (2 * 1024)
 #define DSPAM_CHECK_DATA_UNUM 1l
 #define DSPAM_CHECK_ITERATIONS 128
-#define DSPAM_CHECK_DATA_PATH "res/corpus/"
+#define DSPAM_CHECK_DATA_PATH "../res/corpus/"
 
-#define VIRUS_CHECK_DATA_PATH "res/corpus/"
+#define VIRUS_CHECK_DATA_PATH "../res/corpus/"
 
 // Controls the size of the compression test block.
 #define COMPRESS_CHECK_SIZE_MIN 1024 // 1 kilobyte
@@ -135,16 +135,16 @@ extern int case_timeout;
 #define TANK_CHECK_DATA_UNUM 1l
 #define TANK_CHECK_DATA_MTHREADS 8
 #define TANK_CHECK_DATA_CLEANUP true
-#define TANK_CHECK_DATA_PATH "res/corpus"
+#define TANK_CHECK_DATA_PATH "../res/corpus"
 
 #define DSPAM_CHECK_DATA_UNUM 1l
 #define DSPAM_CHECK_ITERATIONS 8192
 #define DSPAM_CHECK_SIZE_MIN 1024
 #define DSPAM_CHECK_SIZE_MAX (16 * 1024)
 //#define DSPAM_CHECK_SIZE_MAX (1 * 1024 * 1024) // 1 megabyte
-#define DSPAM_CHECK_DATA_PATH "res/corpus"
+#define DSPAM_CHECK_DATA_PATH "../res/corpus"
 
-#define VIRUS_CHECK_DATA_PATH "res/corpus"
+#define VIRUS_CHECK_DATA_PATH "../res/corpus"
 
 #define COMPRESS_CHECK_MTHREADS 8
 #define COMPRESS_CHECK_ITERATIONS 256

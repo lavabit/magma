77a880e97890082d7882f6cc14c7600a7f4893cc
Zachary Michaels <mikezackles@gmail.com>
2015-07-10 17:27:21 -0400

    Style improvements for register.sh

--- scripts/test/json_api/register.sh
+++ scripts/test/json_api/register.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 API_PATH="http://localhost:10000/json"
-JSON="{\
-	\"id\": 1,\
-	\"method\": \"register\",\
-	\"params\": {\
-		\"username\":\"testuser1\",\
-		\"password\":\"testpassword\",\
-		\"password_verification\":\"testpassword\"\
-	}\
-}"
-curl --silent --cookie "$COOKIES" --cookie-jar "$COOKIES" --data "$JSON" "$API_PATH"
+read -d '' JSON <<"EOF"
+{
+	"id": 1,
+	"method": "register",
+	"params": {
+		"username": "testuser1",
+		"password": "testpassword",
+		"password_verification": "testpassword"
+	}
+}
+EOF
+
+echo "$JSON"
+curl --silent --data "$JSON" "$API_PATH"
+echo ""

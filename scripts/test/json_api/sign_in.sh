47b682806b0e6d93dda2b74b8c62639ae6fb64c0
Zachary Michaels <mikezackles@gmail.com>
2015-07-10 19:02:47 -0400

    Improve sign in script

---
+++ scripts/test/json_api/sign_in.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+API_PATH="http://localhost:10000/json"
+read -d '' JSON <<EOF
+{
+	"id": 1,
+	"method": "auth",
+	"params": {
+		"username": "$1",
+		"password": "$2"
+	}
+}
+EOF
+
+echo "Request:"
+echo "$JSON"
+echo "Response:"
+curl --silent --data "$JSON" "$API_PATH"
+echo ""

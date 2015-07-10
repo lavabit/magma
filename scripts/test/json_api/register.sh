86c52c7414f508cbe8d29e626dd5fe5dbb73612e
Zachary Michaels <mikezackles@gmail.com>
2015-07-10 15:54:06 -0400

    Clean up register script

--- scripts/test/json_api/register.sh
+++ scripts/test/json_api/register.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-COOKIES=`mktemp`
 API_PATH="http://localhost:10000/json"
-JSON="{\"id\":1,\"method\":\"register\",\"params\":{\"username\":\"testuser1\",\"password\":\"testpassword\",\"password_verification\":\"testpassword\"}}"
-RESPONSE=`curl --silent --cookie "$COOKIES" --cookie-jar "$COOKIES" --data "$JSON" "$API_PATH"`
-echo $RESPONSE
-rm -f "$COOKIES"
+JSON="{\
+	\"id\": 1,\
+	\"method\": \"register\",\
+	\"params\": {\
+		\"username\":\"testuser1\",\
+		\"password\":\"testpassword\",\
+		\"password_verification\":\"testpassword\"\
+	}\
+}"
+curl --silent --cookie "$COOKIES" --cookie-jar "$COOKIES" --data "$JSON" "$API_PATH"

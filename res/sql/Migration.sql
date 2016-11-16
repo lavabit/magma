946d1644261b6ea16fe671abcc7fe2a9f9e12913
Ladar Levison <ladar@lavabit.com>
2016-11-15 20:11:38 -0600

    Removed a hard coded schema reference inside a SQL init sript.

--- res/sql/Migration.sql
+++ res/sql/Migration.sql
@@ -11,8 +11,8 @@ ALTER TABLE `Folders` DROP INDEX `UNIQ_FOLDERNAME`, ADD UNIQUE INDEX `UNIQ_FOLDE
 /* Alter the Users table for STACIE support. Deprecate any existing legacy passwords. */
 /* ALTER TABLE `Lavabit`.`Users` CHANGE COLUMN `password` `legacy` VARCHAR(130) NULL DEFAULT NULL; */
 
-ALTER TABLE `Lavabit`.`Users` CHANGE COLUMN `ssl` `tls` tinyint(1) NOT NULL DEFAULT '0';
-ALTER TABLE `Lavabit`.`Limits` CHANGE COLUMN `ssl` `tls` tinyint(1) NOT NULL DEFAULT '1';
+ALTER TABLE `Users` CHANGE COLUMN `ssl` `tls` tinyint(1) NOT NULL DEFAULT '0';
+ALTER TABLE `Limits` CHANGE COLUMN `ssl` `tls` tinyint(1) NOT NULL DEFAULT '1';
 
 DROP TABLE `Aliases`;
 

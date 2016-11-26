6a0743a9fd56be45c75e48b4a70590a2e552c4ac
Ladar Levison <ladar@lavabit.com>
2016-11-26 12:12:19 -0600

    More messages for the unit tests.

--- check/magma/data/data_check.c
+++ check/magma/data/data_check.c
@@ -12,9 +12,39 @@
 
 #include "magma_check.h"
 #include "message.1.h"
+#include "message.2.h"
+#include "message.3.h"
+#include "message.4.h"
+#include "message.5.h"
+#include "message.6.h"
+#include "message.7.h"
+#include "message.8.h"
+#include "message.9.h"
+#include "message.10.h"
+#include "message.11.h"
+#include "message.12.h"
+#include "message.13.h"
+#include "message.14.h"
+#include "message.15.h"
+#include "message.16.h"
 
 stringer_t *messages[] = {
-	NULLER(MESSAGE_1)
+	NULLER(MESSAGE_1),
+	NULLER(MESSAGE_2),
+	NULLER(MESSAGE_3),
+	NULLER(MESSAGE_4),
+	NULLER(MESSAGE_5),
+	NULLER(MESSAGE_6),
+	NULLER(MESSAGE_7),
+	NULLER(MESSAGE_8),
+	NULLER(MESSAGE_9),
+	NULLER(MESSAGE_10),
+	NULLER(MESSAGE_11),
+	NULLER(MESSAGE_12),
+	NULLER(MESSAGE_13),
+	NULLER(MESSAGE_14),
+	NULLER(MESSAGE_15),
+	NULLER(MESSAGE_16)
 };
 
 uint32_t check_message_max(void) {

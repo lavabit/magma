6d21318c5c2536e9a45f73ad5cc8c83f183eee3d
Ladar Levison <ladar@lavabit.com>
2017-01-07 05:31:03 -0600

    Updated comments, error handling for field write/size functions.

--- src/providers/prime/formats/fields.c
+++ src/providers/prime/formats/fields.c
@@ -36,9 +36,21 @@ int_t prime_field_size_length(prime_field_type_t field) {
 
 	int_t result = -1;
 
+	// Signature fields are a fixed length, and thus don't include a length parameter. Note that field 253 is the full
+	// signet signature, and field 255 is the identifiable signet signature.
 	if (field == 0 || (field >= 4 && field <= 15) || field == 253 || field == 255) result = 0;
+
+	// The cryptographic fields, and the informational fields between 16 and 159 all use 1 byte for the
+	// length parameter. Note that field 254 is the identifier.
 	else if ((field >= 1 && field <= 3) || (field >= 16 && field <= 159) || field == 254) result = 1;
+
+	// The informational fields between 160 and 250 all use 2 bytes for the length parameter.
 	else if (field >= 160 && field <= 250) result = 2;
+
+	// Field 251 is for undefined fields, and thus currently lacks an a length because it employs an alternate format.
+	/// TODO: else if (field == 251) result = ???;
+
+	// The image field is the only one with a 3 byte length parameter.
 	else if (field == 252) result = 3;
 
 	return result;
@@ -51,11 +63,33 @@ size_t prime_field_size_max(prime_type_t type, prime_field_type_t field) {
 	switch (type) {
 		case (PRIME_ORG_KEY):
 		case (PRIME_ORG_SIGNET):
+			// Invalid and/or reserved fields.
+			if (field == 0 || (field >= 5 && field <= 15)) result = 0;
+			// Fixed length signature fields.
+			else if (field == 4 || field == 253 || field == 255) result = PRIME_FIXED_SIZE;
+			// Fields with a 1 byte length.
+			else if ((field >= 1 && field <= 3) || (field >= 16 && field <= 159) || field == 254) result = PRIME_MAX_1_BYTE;
+			// Fields with a 2 byte length.
+			else if (field >= 160 && field <= 250) result = PRIME_MAX_2_BYTE;
+			// Field 251 is for undefined extensions, and they use a different format.
+			/// TODO: else if (field == 251) result = ???;
+			// Field 252 is holds the image.
+			else if (field == 252) result = PRIME_MAX_3_BYTE;
+			break;
 		case (PRIME_USER_SIGNING_REQUEST):
 		case (PRIME_USER_SIGNET):
 		case (PRIME_USER_KEY):
-			if ((field >= 1 && field <= 3) || (field >= 16 && field <= 159) || field == 254) result = PRIME_MAX_1_BYTE;
+			// Invalid or reserved fields.
+			if (field == 0 || (field >= 7 && field <= 15)) result = 0;
+			// Fixed length signature fields.
+			else if ((field >= 4 && field <= 6) || field == 253 || field == 255) result = PRIME_FIXED_SIZE;
+			// Fields with a 1 byte length.
+			else if ((field >= 1 && field <= 3) || (field >= 16 && field <= 159) || field == 254) result = PRIME_MAX_1_BYTE;
+			// Fields with a 2 byte length.
 			else if (field >= 160 && field <= 250) result = PRIME_MAX_2_BYTE;
+			// Field 251 is for undefined extensions, and they use a different format.
+			/// TODO: else if (field == 251) result = ???;
+			// Field 252 is holds the image.
 			else if (field == 252) result = PRIME_MAX_3_BYTE;
 			break;
 		case (PRIME_ORG_KEY_ENCRYPTED):
@@ -64,6 +98,7 @@ size_t prime_field_size_max(prime_type_t type, prime_field_type_t field) {
 			break;
 		default:
 			log_pedantic("Unrecognized PRIME type.");
+			break;
 	}
 
 	return result;
@@ -75,6 +110,12 @@ stringer_t * prime_field_write(prime_type_t type, prime_field_type_t field, size
 	stringer_t *result = NULL;
 	uint32_t big_endian_size = 0;
 	size_t total = 0, size_len = 0, payload_len = 0;
+
+	/// TODO: Add undefined field support.
+	if (field == 251) {
+		log_pedantic("We don't handle undefined fields, yet.");
+		return NULL;
+	}
 
 	// Figure out how big the field heading is.
 	if (!(size_len = prime_field_size_length(field)) || st_empty_out(data, &payload, &payload_len)) {

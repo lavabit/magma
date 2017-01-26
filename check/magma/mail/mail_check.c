6673711445134245a3ac511b36e3cd20326e7453
Ladar Levison <ladar@lavabit.com>
2017-01-26 13:31:51 -0600

    Fixed memory leak in mail load test when forking.

--- check/magma/mail/mail_check.c
+++ check/magma/mail/mail_check.c
@@ -13,6 +13,11 @@ START_TEST (check_mail_load_s) {
 
 	if (status()) result = check_mail_load_sthread(errmsg);
 
+	// Because libcheck will sometimes fork the process to protect against segmentation faults
+	// the normal thread cleanup code won't be called, so we do it explicitly here to avoid valgrind
+	// complaints about a memory leak.
+	mail_cache_reset();
+
 	log_test("MAIL / LOAD / SINGLE THREADED:", errmsg);
 	ck_assert_msg(result, st_char_get(errmsg));
 }

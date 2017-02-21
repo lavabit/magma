27e90bd78caf98dd8425082e2d950913241a746a
Jacob Adkins <jacob@sidekick.localdomain>
2017-02-20 18:39:34 -0600

    Cleaned up engine_check.c

--- check/magma/engine/engine_check.c
+++ check/magma/engine/engine_check.c
@@ -19,16 +19,6 @@ START_TEST (check_engine_context_system_s) {
 			st_sprint(errmsg, "System limit check failed. { control = CTL_FS / FS_MAXFILE }");
 			result = false;
 		}
-//
-//		log_enable();
-//
-//		stringer_t *path = st_quick(MANAGEDBUF(255), "/proc/%i/fd/", process_my_pid());
-//		log_pedantic("path = %.*s", st_length_int(path), st_char_get(path));
-//		log_pedantic("before = %i", folder_count(path, false, false));
-//		stringer_t *m = st_alloc_opts(MAPPED_T | JOINTED | HEAP, 1024);
-//		log_pedantic("after = %i", folder_count(path, false, false));
-//		st_free(m);
-//		log_pedantic("postfree = %i", folder_count(path, false, false));
 
 		magma.config.output_resource_limits = true;
 		if (result && !system_init_resource_limits()) {

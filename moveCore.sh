5dec163b243532e0df328de25005510ae7427019
Magma Developer <garrett@garrettbattaglia.com>
2018-04-25 13:41:12 -0500

    improved moveCore.sh by	using same commit message as the last commit to	magma. also added a warning if	core is	left in	an unbuildable state

--- moveCore.sh
+++ moveCore.sh
@@ -5,5 +5,11 @@ cp -R  ./src/core/ ./../libcore/src
 cp -R ./check/magma/core/ ./../libcore/check
 cd ./../libcore
 git add .
-git commit
+git commit -a -m "$(git show -s --format='%s' | cat)"
 git push
+
+cd ./../libcore
+if [ !make ]; then
+	echo "WARNING: core is in a broken state and wont build. Please fix."
+fi
+cd ./../magma

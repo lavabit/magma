287eb5d5077812ec9523239bb7f93dbf38c5817c
Ladar Levison <ladar@lavabit.com>
2018-08-20 05:25:37 -0500

    Added functions for getting specific components of the magma version.

--- src/engine/status/build.c
+++ src/engine/status/build.c
@@ -43,3 +43,50 @@ const char * build_stamp(void) {
 	return (const char *)MAGMA_TIMESTAMP;
 }
 
+/**
+ * @brief	Get the magma major version number.
+ * @return	an unsigned integer with the major version number.
+ */
+uint64_t build_version_major(void) {
+
+	uint64_t major = 0;
+	placer_t holder = pl_null();
+
+	// Try to extract the major version. If anything goes wrong, return the initialized value of 0.
+	if (tok_get_st(PLACER(MAGMA_VERSION, ns_length_get(MAGMA_VERSION)), '.', 0, &holder) >= 0)
+		uint64_conv_pl(holder, &major);
+
+	return major;
+}
+
+/**
+ * @brief	Get the magma minor version number.
+ * @return	an unsigned integer with the minor version number.
+ */
+uint64_t build_version_minor(void) {
+
+	uint64_t minor = 0;
+	placer_t holder = pl_null();
+
+	// Try to extract the minor version. If anything goes wrong, return the initialized value of 0.
+	if (tok_get_st(PLACER(MAGMA_VERSION, ns_length_get(MAGMA_VERSION)), '.', 1, &holder) >= 0)
+		uint64_conv_pl(holder, &minor);
+
+	return minor;
+}
+
+/**
+ * @brief	Get the magma patch level.
+ * @return	an unsigned integer with the patch level.
+ */
+uint64_t build_version_patch(void) {
+
+	uint64_t patch = 0;
+	placer_t holder = pl_null();
+
+	// Try to extract the patch level. If anything goes wrong, return the initialized value of 0.
+	if (tok_get_st(PLACER(MAGMA_VERSION, ns_length_get(MAGMA_VERSION)), '.', 2, &holder) >= 0)
+		uint64_conv_pl(holder, &patch);
+
+	return patch;
+}
--- src/engine/status/status.h
+++ src/engine/status/status.h
@@ -8,15 +8,37 @@
 #ifndef MAGMA_ENGINE_STATUS_H
 #define MAGMA_ENGINE_STATUS_H
 
-/************  BUILD  ************/
-const char * build_stamp(void);
-const char * build_commit(void);
-const char * build_version(void);
-/************  BUILD  ************/
+/// statistics.c
+void       stats_adjust_by_name(char *name, int32_t value);
+void       stats_adjust_by_num(uint64_t position, int32_t value);
+void       stats_decrement_by_name(char *name);
+void       stats_decrement_by_num(uint64_t position);
+uint64_t   stats_derived_count(void);
+char *     stats_derived_name(uint64_t position);
+uint64_t   stats_derived_value(uint64_t position);
+uint64_t   stats_get_count(void);
+char *     stats_get_name(uint64_t position);
+uint64_t   stats_get_name_pos(char *name);
+uint64_t   stats_get_value_by_name(char *name);
+uint64_t   stats_get_value_by_num(uint64_t position);
+void       stats_increment_by_name(char *name);
+void       stats_increment_by_num(uint64_t position);
+bool_t     stats_init(void);
+void       stats_set_by_name(char *name, uint64_t value);
+void       stats_set_by_num(uint64_t position, uint64_t value);
+void       stats_shutdown(void);
+uint64_t   stats_sum_errors(void);
 
-/************  PERFORMANCE  ************/
-uint64_t perf_rdtsc(void);
-/************  PERFORMANCE  ************/
+/// performance.c
+uint64_t   perf_rdtsc(void);
+
+/// build.c
+const      char * build_commit(void);
+const      char * build_stamp(void);
+const      char * build_version(void);
+uint64_t   build_version_major(void);
+uint64_t   build_version_minor(void);
+uint64_t   build_version_patch(void);
 
 /// status.c
 bool_t     status(void);
@@ -26,35 +48,5 @@ void       status_process(void);
 void       status_set(int value);
 void       status_signal(void);
 uint64_t   status_startup(void);
-
-/************  STATISTICS  ************/
-uint64_t stats_derived_count (void);
-char *stats_derived_name (uint64_t position);
-uint64_t stats_derived_value (uint64_t position);
-
-bool_t stats_init(void);
-void stats_shutdown(void);
-
-uint64_t stats_get_count(void);
-char * stats_get_name(uint64_t position);
-
-uint64_t stats_get_value_by_name(char *name);
-uint64_t stats_get_value_by_num(uint64_t position);
-
-void stats_decrement_by_name(char *name);
-void stats_decrement_by_num(uint64_t position);
-
-void stats_increment_by_name(char *name);
-void stats_increment_by_num(uint64_t position);
-
-void stats_set_by_name(char *name, uint64_t value);
-void stats_set_by_num(uint64_t position, uint64_t value);
-
-void stats_adjust_by_name(char *name, int32_t value);
-void stats_adjust_by_num(uint64_t position, int32_t value);
-/************  STATISTICS  ************/
-
-stringer_t *  host_platform(stringer_t *output);
-stringer_t *  host_version(stringer_t *output);
 
 #endif

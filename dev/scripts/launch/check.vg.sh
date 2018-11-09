093d35a04ac9262ac1d1c752f1a47996b470db22
Ladar Levison <ladar@lavabit.com>
2018-11-09 01:30:57 -0600

    Updated the valgrind check script for single checks.

--- dev/scripts/launch/check.vg.sh
+++ dev/scripts/launch/check.vg.sh
@@ -14,8 +14,8 @@ MAGMA_DIST=`pwd`
 
 # add for suppressions --gen-suppressions=all
 # self modifying code --smc-check=[none,stack,all]
-# track file descriptors --track-fds=yes
 
+if [ $# == 1 ]; then
 valgrind --tool=memcheck \
 --log-fd=1 \
 --trace-children=yes \
@@ -27,6 +27,30 @@ valgrind --tool=memcheck \
 --show-below-main=no \
 --undef-value-errors=yes \
 --track-origins=yes \
+--track-fds=yes \
+--read-var-info=yes \
+--smc-check=none \
+--fullpath-after= \
+--leak-check=yes \
+--show-reachable=yes \
+--leak-resolution=high \
+--workaround-gcc296-bugs=no \
+--partial-loads-ok=yes \
+--suppressions=$MAGMA_DIST/sandbox/etc/magma.suppressions \
+$MAGMA_DIST/magmad.check --check $1  $MAGMA_DIST/sandbox/etc/magma.sandbox.config
+else
+valgrind --tool=memcheck \
+--log-fd=1 \
+--trace-children=yes \
+--child-silent-after-fork=yes \
+--run-libc-freeres=yes \
+--demangle=yes \
+--num-callers=50 \
+--error-limit=no \
+--show-below-main=no \
+--undef-value-errors=yes \
+--track-origins=yes \
+--track-fds=yes \
 --read-var-info=yes \
 --smc-check=none \
 --fullpath-after= \
@@ -37,3 +61,5 @@ valgrind --tool=memcheck \
 --partial-loads-ok=yes \
 --suppressions=$MAGMA_DIST/sandbox/etc/magma.suppressions \
 $MAGMA_DIST/magmad.check $MAGMA_DIST/sandbox/etc/magma.sandbox.config
+fi
+

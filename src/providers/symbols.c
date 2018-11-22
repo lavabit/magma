d6d38cdc8eaee30a911e557d97c35b18936d0ac0
Ladar Levison <ladar@lavabit.com>
2018-11-22 00:16:58 -0600

    Ignore config file log options when printing the init banner.

--- src/providers/symbols.c
+++ src/providers/symbols.c
@@ -684,7 +684,8 @@ bool_t lib_load(void) {
 		return false;
 	}
 
-	log_pedantic("-------------------------------- VERSIONS --------------------------------\n\n" \
+	log_options(M_LOG_PEDANTIC | M_LOG_TIME_DISABLE | M_LOG_FILE_DISABLE | M_LOG_LINE_DISABLE | M_LOG_FUNCTION_DISABLE | M_LOG_STACK_TRACE_DISABLE, \
+		"-------------------------------- VERSIONS --------------------------------\n\n" \
 		"%-10.10s %63.63s\n%-10.10s %63.63s\n%-10.10s %63.63s\n\n" \
 		"%-10.10s %63.63s\n%-10.10s %63.63s\n\n" \
 		"%-10.10s %63.63s\n%-10.10s %63.63s\n%-10.10s %63.63s\n%-10.10s %63.63s\n\n" \

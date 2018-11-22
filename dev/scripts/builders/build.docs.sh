d4b2684b8e647f4cd1f8308dfe955ff1f899e732
Ladar Levison <ladar@lavabit.com>
2018-11-21 22:17:32 -0600

    Correct syntax typo introduced by the path resolution logic commit.

--- dev/scripts/builders/build.docs.sh
+++ dev/scripts/builders/build.docs.sh
@@ -4,7 +4,8 @@
 if [[ $0 != $BASH_SOURCE ]]; then
   export CMD=`readlink -f $BASH_SOURCE`
 else
-  export CMD=`readlink -f $0`fi
+  export CMD=`readlink -f $0`
+fi
 
 # Cross Platform Base Directory Discovery
 pushd `dirname $CMD` > /dev/null
--- dev/scripts/parsers/queries.sh
+++ dev/scripts/parsers/queries.sh
@@ -11,7 +11,8 @@
 if [[ $0 != $BASH_SOURCE ]]; then
   export CMD=`readlink -f $BASH_SOURCE`
 else
-  export CMD=`readlink -f $0`fi
+  export CMD=`readlink -f $0`
+fi
 
 # Cross Platform Base Directory Discovery
 pushd `dirname $CMD` > /dev/null

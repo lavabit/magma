791da149b071be37b15c422495e6052ddc19c547
Ladar Levison <ladar@lavabit.com>
2022-05-09 01:57:25 -0500

    Updated ClamAV to 0.103.6.

--- dev/scripts/builders/build.lib.sh
+++ dev/scripts/builders/build.lib.sh
@@ -1328,29 +1328,29 @@ clamav() {
         
         # Output the version number and not the git commit hash.
         cat "$M_PATCHES/clamav/"version_0984.patch | patch -p1 --verbose &>> "$M_LOGS/clamav.txt"; error
-      elif [[ $CLAMAV =~ "clamav-0.103.5" ]]; then 
+      elif [[ $CLAMAV =~ ^clamav-0.103.[5-6]$ ]]; then 
         # Add the shutdown and clean up functions and fix the rar library dynamic loading logic.
-        cat "$M_PATCHES/clamav/"shutdown_rarload_01035.patch | patch -p1 --verbose &>> "$M_LOGS/clamav.txt"; error
+        cat "$M_PATCHES/clamav/"shutdown_rarload_01035.patch | patch -p1 --fuzz=100 --verbose &>> "$M_LOGS/clamav.txt"; error
 
         # Add the ability to dictate the CA bundle file location when running freshclam.
-        cat "$M_PATCHES/clamav/"freshclam_cafile_option_01035.patch | patch -p1 --verbose &>> "$M_LOGS/clamav.txt"; error
+        cat "$M_PATCHES/clamav/"freshclam_cafile_option_01035.patch | patch -p1 --fuzz=100 --verbose &>> "$M_LOGS/clamav.txt"; error
         
         # Output the version number and not the git commit hash.
-        cat "$M_PATCHES/clamav/"version_0984.patch | patch -p1 --verbose &>> "$M_LOGS/clamav.txt"; error
+        cat "$M_PATCHES/clamav/"version_0984.patch | patch -p1 --fuzz=100 --verbose &>> "$M_LOGS/clamav.txt"; error
 
         # Fixes pattern matching and logic bugs in the clamd unit test, so we avoid check failures on some the affected platforms. 
-      	cat "$M_PATCHES/clamav/"check_clamd_fixes_01035.patch | patch -p1 --verbose &>> "$M_LOGS/clamav.txt"; error
+      	cat "$M_PATCHES/clamav/"check_clamd_fixes_01035.patch | patch -p1 --fuzz=100 --verbose &>> "$M_LOGS/clamav.txt"; error
       	
       # Applied to most recent version.
       else
         # Add the shutdown and clean up functions and fix the rar library dynamic loading logic.
-        cat "$M_PATCHES/clamav/"shutdown_rarload_01042.patch | patch -p1 --verbose &>> "$M_LOGS/clamav.txt"; error
+        cat "$M_PATCHES/clamav/"shutdown_rarload_01042.patch | patch -p1 --fuzz=100 --verbose &>> "$M_LOGS/clamav.txt"; error
 
         # Add the ability to dictate the CA bundle file location when running freshclam.
-        cat "$M_PATCHES/clamav/"freshclam_cafile_option_01042.patch | patch -p1 --verbose &>> "$M_LOGS/clamav.txt"; error
+        cat "$M_PATCHES/clamav/"freshclam_cafile_option_01042.patch | patch -p1 --fuzz=100 --verbose &>> "$M_LOGS/clamav.txt"; error
         
         # Output the version number and not the git commit hash.
-        cat "$M_PATCHES/clamav/"version_0984.patch | patch -p1 --verbose &>> "$M_LOGS/clamav.txt"; error
+        cat "$M_PATCHES/clamav/"version_0984.patch | patch -p1 --fuzz=100 --verbose &>> "$M_LOGS/clamav.txt"; error
         
         # These fixes may not be required with 0.104.2.
         # Fixes pattern matching and logic bugs in the clamd unit test, so we avoid check failures on some the affected platforms. 

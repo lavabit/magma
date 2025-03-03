12febb78223de1611deb9dea87744ff3fbe8a8b6
Ladar Levison <ladar@lavabit.com>
2025-03-03 08:07:16 -0600

    Adding clobbered virus databases to speed up testing.

--- dev/scripts/freshen/clobber.clamav.sh
+++ dev/scripts/freshen/clobber.clamav.sh
@@ -21,6 +21,8 @@ popd > /dev/null
 
 cd $BASE/../../../
 
+export MAGMA_DIST=`pwd`
+
 # If the TERM environment variable is missing, then tput may trigger a fatal error.
 if [[ -n "$TERM" ]] && [[ "$TERM" != "dumb" ]]; then
   export TPUT="tput"
@@ -28,20 +30,10 @@ else
   export TPUT="tput -Tvt100"
 fi
 
-export MAGMA_DIST=`pwd`
-export LD_LIBRARY_PATH=$MAGMA_DIST/lib/sources/clamav/libclamav/.libs/:$MAGMA_DIST/lib/sources/clamav/libfreshclam/.libs/
-
 # If the sandbox contains a CA bundle file, override the path to the CA file used by Freshclam.
-if [ -f $MAGMA_DIST/sandbox/etc/ca-bundle.crt ]; then
-  lib/sources/clamav/freshclam/.libs/freshclam --user $USER --ca=$MAGMA_DIST/sandbox/etc/ca-bundle.crt --datadir=$MAGMA_DIST/sandbox/virus --config-file=$MAGMA_DIST/sandbox/etc/freshclam.conf
-else
-  lib/sources/clamav/freshclam/.libs/freshclam --user $USER --datadir=$MAGMA_DIST/sandbox/virus --config-file=$MAGMA_DIST/sandbox/etc/freshclam.conf
-fi
-
-# Print statistics for all of the ClamAV database files.
-printf "\n\n"
-find sandbox/virus/ \( -type f -name "*.cvd" -print0 \) -or \( -type f -name "*.cld" -print0 \) | xargs -0 --replace={} bash -c '$MAGMA_DIST/lib/sources/clamav/sigtool/sigtool --info={} | grep -v Builder | grep -v MD5 | grep -v "Digital signature" ; printf "\n"'
+cp -f  "$MAGMA_DIST/dev/main.cvd" "$MAGMA_DIST/sandbox/virus/main.cvd"
+cp -f  "$MAGMA_DIST/dev/daily.cvd" "$MAGMA_DIST/sandbox/virus/daily.cvd"
+cp -f  "$MAGMA_DIST/dev/bytecode.cvd" "$MAGMA_DIST/sandbox/virus/bytecode.cvd"
 
 # Done.
 [[ -t 0 ]] && ${TPUT} setaf 2 || true ; printf "Done.\n" ; [[ -t 0 ]] && ${TPUT} sgr0 || true
-
